c7084434da0ca7bfab9f9a4f18ae7d7f3187e6cb
pixelwebdevelopers <pixelwebdevelopers1@gmail.com>
2026-08-22 03:02:56 +0500

    google analytics tag added

--- app/layout.tsx
+++ app/layout.tsx
@@ -154,6 +154,20 @@ export default function RootLayout({
       >
         {children}
         <FloatingBMC />
+        {/* Google tag (gtag.js) */}
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-47SR401CP0"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'G-47SR401CP0');
+          `}
+        </Script>
         <Script
           id="ld-org"
           type="application/ld+json"
